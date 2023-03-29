54a92a2d5f0ea7f6291e260cbb57341de176daef
Manuel Walter Navarro Zeta <manuel08n@gmail.com>
2023-03-29 08:13:59 -0500

    Modified JS

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import { myRegex } from "./methodtest.js";
+import * as methodTest from "./methodtest.js";
+import solved from "./methodtest.js";
 
-console.log(myRegex("manu code red power"));
+// Encuentra al dev
+console.log(methodTest.myRegex("manu dev code red power"));
+solved('Manuel')
+// Encuentra a Waldo
+console.log(methodTest.searchGame('Somewhere Waldo is hiding in this text.'))
+
--- methodtest.js
+++ methodtest.js
@@ -1,4 +1,22 @@
 const regex = /dev/;
+const searchRegex = /Waldo/;
 const myRegex = (myString) => regex.test(myString);
 
-export { myRegex };
+export default function solved(myString) {
+  console.log(myString.length);
+}
+// el método test no funciona si la palabra a buscar es diferente
+// Encuentra a waldo
+const searchGame = (myString) => {
+  if (searchRegex.test(myString)) {
+    return "Enhorabuena encontraste a Waldo";
+  }
+  return "Lo siento Waldo no se encuentra en tu string";
+};
+const boolIf = (bool) => bool;
+export { myRegex, searchGame };
+
+// Multiples patrones
+// Encuentra a la mascota de shagui
+const petReguex = /Scooby|Doo/;
+const mysteryMachine = (string) => {};
